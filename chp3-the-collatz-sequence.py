766f900f2a312481fc1f1f498e7d11e1cd41a150
Sally Ragab <sallyragab31@gmail.com>
2019-06-21 09:36:52 +0200

    Create chp3-the-collatz-sequence.py

---
+++ chp3-the-collatz-sequence.py
@@ -0,0 +1,10 @@
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    elif number % 2 == 1:
+        print(3 * number + 1)
+        return 3 * number + 1
+        
+while collatz(int(input())) != 1:
+    collatz(int(input()))
